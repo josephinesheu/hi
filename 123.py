7743b400a56b25077e3937d2b17848a20619edd5
Josephine Hsu <josephinesheu@gmail.com>
2021-06-28 15:48:42 +0800

    Add new file

---
+++ 123.py
@@ -0,0 +1,14 @@
+price =10
+pen=0.38
+rain=True
+name='josephine'
+x=7
+print(price, pen, rain, name, x)
+name=input('請輸入名字:')
+print('嗨', name)
+height=input('請輸入身高: ')
+y=input('請繼續輸入')
+print=('hello',y)
+weight=input('請輸入體重: ')
+
+
